7f4a3ee42e690d175df2512d5c70d5ad0bc0bf8b
Mirian Shilakadze <mirian.shilakadze@gmail.com>
2023-11-22 20:54:33 +0400

    Move direction to seperated files

---
+++ include/core/direction.h
@@ -0,0 +1,19 @@
+#ifndef CAVE_DIRECTION_H
+#define CAVE_DIRECTION_H
+
+#include "../util.h"
+
+typedef enum Direction {
+    DIR_NORTH = 0,
+    DIR_EAST,
+    DIR_SOUTH,
+    DIR_WEST,
+    DIR_TOP,
+    DIR_BOTTOM,
+
+	DIR_NULL
+} Direction;
+
+void direction_get_norm(Direction dir, Vec3i *dest);
+
+#endif
---
+++ src/core/direction.c
@@ -0,0 +1,38 @@
+#include "../../include/core/direction.h"
+
+void direction_get_norm(Direction dir, Vec3i *dest)
+{
+	zinc_vec3i_zero(dest);
+
+	switch(dir){
+
+	case DIR_NORTH:
+		dest->z += 1;
+		break;
+
+	case DIR_EAST:
+		dest->x += 1;
+		break;
+
+	case DIR_SOUTH:
+		dest->z -= 1;
+		break;
+
+	case DIR_WEST:
+		dest->x -= 1;
+		break;
+
+	case DIR_TOP:
+		dest->y += 1;
+		break;
+
+	case DIR_BOTTOM:
+		dest->y -= 1;
+		break;
+
+	default:
+		break;
+
+	}
+
+}
