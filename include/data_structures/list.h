fa021ff3dd5fd9697e6684822ab6f953b1eb9732
Mirian Shilakadze <mirian.shilakadze@gmail.com>
2024-12-15 23:53:57 +0400

    Change linked lists

---
+++ include/data_structures/list.h
@@ -0,0 +1,35 @@
+#ifndef CAVE_LINKED_LIST_H
+#define CAVE_LINKED_LIST_H
+
+#include "util.h"
+
+#define list_for_each(_head, _pos) \
+    for ((_pos) = (_head)->next; (_pos) != (_head); (_pos) = (_pos)->next)
+
+#define list_for_each_rev(_head, _pos) \
+    for ((_pos) = (_head)->prev; (_pos) != (_head); (_pos) = (_pos)->prev)
+
+#define list_for_each_safe(_head, _pos, _next)              \
+    for ((_pos) = (_head)->next, (_next) = (_pos)->next;    \
+         (_pos) != (_head);                                 \
+         (_pos) = (_next), (_next) = (_pos)->next)
+
+#define list_for_each_rev_safe(_head, _pos, _next)          \
+    for ((_pos) = (_head)->prev, (_next) = (_pos)->prev;    \
+         (_pos) != (_head);                                 \
+         (_pos) = (_next), (_next) = (_pos)->prev)
+
+typedef struct ListNode {
+    struct ListNode *next;
+    struct ListNode *prev;
+} ListNode;
+
+void list_create(ListNode *head);
+void list_destroy(ListNode *head, void (*free_node)(ListNode *node));
+
+void list_add(ListNode *head, ListNode *node);
+void list_add_tail(ListNode *head, ListNode *node);
+
+void list_del(ListNode *node);
+
+#endif
---
+++ src/data_structures/list.c
@@ -0,0 +1,39 @@
+#include "data_structures/list.h"
+
+void list_create(ListNode *head)
+{
+    head->next = head;
+    head->prev = head;
+}
+void list_destroy(ListNode *head, void (*free_node)(ListNode *node))
+{
+    ListNode *pos, *next;
+
+    list_for_each_safe(head, pos, next) {
+        list_del(pos);
+        free_node(pos);
+    }
+}
+
+void list_add(ListNode *head, ListNode *node)
+{
+    head->next->prev = node;
+    node->next = head->next;
+    node->prev = head;
+    head->next = node;
+}
+
+void list_add_tail(ListNode *head, ListNode *node)
+{
+    head->prev->next = node;
+    node->prev = head->prev;
+    node->next = head;
+    head->prev = node;
+}
+
+void list_del(ListNode *node)
+{
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+}
+
